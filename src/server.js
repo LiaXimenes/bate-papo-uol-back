c83ed18db7ddbe57549c5e9a7e8c8d37acac4230
Lia Ximenes <ximeneslia@gmail.com>
2021-06-11 17:33:22 -0300

    finalização do post status

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ server.get("/participants", (request, response) =>{
 server.post("/participants", (request, response) => {
     const participant = request.body.name;
     if (participant === ""){
-        response.status(400)
+        response.sendStatus(400)
     } else {
         participants.push({name: participant, lastStatus: Date.now()});
         messages.push({
@@ -26,29 +26,41 @@ server.post("/participants", (request, response) => {
             type: 'status',
             time: dayjs().format('HH:mm:ss')
         })
-        response.status(200);
+        response.sendStatus(200);
     }    
 })
 
 server.post("/messages", (request, response) => {
     const user = request.headers.user;
+    
     if(request.body.to === "" || request.body.text === ""){
-        response.status(400);
+        response.sendStatus(400);
 
+    // } else if(tipo de mensagem !== 'mensagem'){
+    //     response.sendStatus(400);  
     } else{
-        messages.push({
-            from: user,
-            to: request.body.to,
-            text: request.body.text,
-            type:request.body.type,
-            time: dayjs().format('HH:mm:ss') 
-        });
-        response.status(200);
+        for (let i = 0; i < participants.length; i++){
+            if(participants[i].name.includes(user)){
+                messages.push({
+                    from: user,
+                    to: request.body.to,
+                    text: request.body.text,
+                    type:request.body.type,
+                    time: dayjs().format('HH:mm:ss') 
+                });
+                response.sendStatus(200);
+                
+            } else{
+                response.sendStatus(400);
+            }
+        }
     }
 })
 
 server.get("/messages", (request, response) =>{
     const user = request.headers.user;
+    // const quantity  = request.query;
+    // console.log(quantity);
 
     response.send(messages);
     
@@ -57,9 +69,15 @@ server.get("/messages", (request, response) =>{
 server.post("/status", (request, response) =>{
     const user = request.headers.user;
     console.log(user);
-    response.send(user)
-    //if(participants){}
-    
+    for (let i = 0; i < participants.length; i++){
+        if(participants[i].name.includes(user)){
+            participants[i].lastStatus = Date.now();
+            response.sendStatus(200);
+            
+        } else{
+            response.sendStatus(400);
+        }
+    }
 })
 
 server.listen(4000);
